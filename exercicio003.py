e0ea306123f3324c2c31d9e1175313d2e8708b69
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2022-07-21 17:43:45 -0300

    Adicionei exercicio003

---
+++ exercicio003.py
@@ -0,0 +1,19 @@
+"""
+Exercícios
+
+1) Mude a ordem das cores
+2) Mude/defina a forma da tartaruga
+3) Mude a largura da linha
+4) Faça a tartaruga desenhar dois quadrados
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.pensize(5)
+
+for color in ['blue', 'black', 'red', 'pink']:
+    turtle.color(color)
+    turtle.forward(100)
+    turtle.right(90)
+
