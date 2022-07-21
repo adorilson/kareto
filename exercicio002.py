cfd56673a5c10b81171f08c4ec74ecb278f6918c
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2022-07-21 17:43:37 -0300

    Adicionei exercicio002

---
+++ exercicio002.py
@@ -0,0 +1,21 @@
+"""
+Exercícios
+
+1) Mude a cor da linha
+2) Mude o formato da tartaruga
+3) Mude o comprimento do quadrado
+4) Mude a forma da tartaruga
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.color('red')
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
