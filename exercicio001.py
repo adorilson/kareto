a267209309e0b9b10e61756c3cf8991523a90077
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2022-07-21 17:38:30 -0300

    Editei exercicios de exercicios001

--- exercicio001.py
+++ exercicio001.py
@@ -3,9 +3,8 @@ Exercícios
 1) Reduza as duas linhas com forward para apenas uma, mas que faça a mesma
 coisa que as atuais
 2) Faça a tartaruga virar para a direita
-3) Mude a forma da tartaruga
-4) Altere os lados do quadrado
-5) Transforme o quadrado em um retângulo
+3) Altere o comprimento dos lados do quadrado
+4) Transforme o quadrado em um retângulo
 """
 
 import turtle
