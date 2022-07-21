16f9e3532105104290ac898beee6a7cf16d3025c
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2022-07-21 18:14:45 -0300

    Adicionei exercicio006 (envelope errado)

---
+++ exercicio006.py
@@ -0,0 +1,18 @@
+"""
+Exercicios
+
+1) Era para ser um envelope. Mas saiu errado. Corrija.
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.color('green')
+
+for _ in [1, 2, 3]:
+    turtle.forward(100)
+    turtle.right(120)
+
+for _ in [1, 2, 3, 4]:
+  turtle.left(90)
+  turtle.forward(100)
