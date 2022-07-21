d0a6429686624a7caea049937396fff74353db5a
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2022-07-21 18:31:05 -0300

    Adicionei exercicio007 (oculos)

---
+++ exercicio007.py
@@ -0,0 +1,24 @@
+"""
+Exercicios:
+
+1) Mude a distancia entres as lentes dos óculos
+2) Mude o tamanho das lentes
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.color('green')
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.backward(100)
+
+turtle.backward(50)
+turtle.backward(100)
+turtle.right(90)
+
+for _ in range(3):
+  turtle.forward(100)
+  turtle.left(90)
+
