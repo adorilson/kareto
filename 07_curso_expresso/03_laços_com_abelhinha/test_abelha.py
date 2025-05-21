65a090e0017286a718fd76bcb31851b10099e409
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2025-05-21 10:07:01 +0100

    [07] Uso de fixture no teste de Abelha

--- 07_curso_expresso/03_laços_com_abelhinha/test_abelha.py
+++ 07_curso_expresso/03_laços_com_abelhinha/test_abelha.py
@@ -1,35 +1,37 @@
+import pytest
+
 from atores import Abelha, DIRECAO, ABELHA_LESTE, ABELHA_OESTE, ABELHA_NORTE, ABELHA_SUL
 
 
-def test_abelha_propriedades_publicas():
-    bee = Abelha()
+@pytest.fixture
+def bee():
+    return Abelha()
+
+def test_abelha_propriedades_publicas(bee):
     assert bee.posicao == 0
 
 
-def test_abelha_vire_oeste():
-    bee = Abelha()
+def test_abelha_vire_oeste(bee):
     bee.vire_oeste()
     assert bee.heading() == DIRECAO.OESTE
     assert bee.shape() == ABELHA_OESTE
 
 
-def test_abelha_vire_norte():
-    bee = Abelha()
+def test_abelha_vire_norte(bee):
     bee.vire_norte()
     assert bee.heading() == DIRECAO.NORTE
     assert bee.shape() == ABELHA_NORTE
 
 
-def test_abelha_vire_leste():
-    bee = Abelha()
+def test_abelha_vire_leste(bee):
     bee.vire_leste()
     assert bee.heading() == DIRECAO.LESTE
     assert bee.shape() == ABELHA_LESTE
 
 
-def test_abelha_avance():
-    bee = Abelha()
+def test_abelha_avance(bee):
     bee.posicao = 10
+
     bee.vire_oeste()
     bee.avance()
     assert bee.posicao == 9
@@ -39,8 +41,7 @@ def test_abelha_avance():
     assert bee.posicao == 1
 
 
-def test_abelha_direita():
-    bee = Abelha()
+def test_abelha_direita(bee):
     bee.vire_oeste()
 
     bee.direita()
