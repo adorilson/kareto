a8629a0014d48743264168a5f53d4fac302e70cd
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2026-04-24 10:14:50 +0100

    [Web] Adiciona modulo de teste especifico para a saída e move os testes existentes pra lá

--- web/tests/test_interacoes.py
+++ web/tests/test_interacoes.py
@@ -42,59 +42,6 @@ def test_captura_automatica():
         assert g3.is_hidden()
 
 
-def test_saida_tarefa_concluida_com_sucesso():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
-        page = browser.new_page()
-        page.goto("http://localhost:8000/?maia=1,4,0&cag=1&gs=3,4&gs=5,4&fast=1")
-
-        assert page.locator('.CodeMirror').is_visible()
-        data = """for _ in range(5): maia.avance()"""
-        page.evaluate('data => {window.editor.setValue(data)}', data)
-
-        page.locator("#run-btn").click()
-        page.wait_for_function("() => window.is_running === false && window.command_queue_len === 0")
-
-        output = page.locator("#output-content").inner_text()
-        assert "Análise de código concluída sem erros de sintaxe." in output
-        assert "Executando o código. Aguarde..." in output
-        assert "Tarefa concluída com sucesso!" in output
-
-
-def test_print42_editor():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
-        page = browser.new_page()
-        page.goto("http://localhost:8000")
-
-        assert page.locator('.CodeMirror').is_visible()
-
-        data = 'print(42)'
-        page.evaluate('data => {window.editor.setValue(data)}', data)
-        page.click("#run-btn")
-
-        assert page.locator("#output-content").inner_text() == '42\n'
-
-
-def test_print42_console():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
-        page = browser.new_page()
-        page.goto("http://localhost:8000")
-
-        page.locator("#console").click()
-        page.keyboard.type("print(42)")
-        page.keyboard.press("Enter")
-
-        # A documentação do Playwright recomenda usar `expect..to_have_text` no
-        # lugar de `inner_text`, mas isso não funcionou para mim.
-        assert page.locator('//*[@id="console"]/pre[5]').is_visible()
-        assert page.locator('//*[@id="console"]/pre[5]').inner_text() == '42'
-
-        assert page.locator('//*[@id="console"]/pre[7]').is_visible()
-        assert page.locator('//*[@id="console"]/pre[7]').inner_text() == '>>> '
-
-
 def test_recomecar():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
@@ -160,31 +107,3 @@ def test_recomecar_com_nectar():
         g2      = assert_ator(page, '#actors > div:nth-child(3)', x=4, y=3, z_index=1, img_src="img/girassol.gif")
 
 
-def test_sem_erro_ao_girar_apos_coleta_automatica():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
-        page = browser.new_page()
-        page.goto("http://localhost:8000/?maia=1,3,0&gs=3,4&gs=1,5&cag=1&fast=1")
-
-        page.wait_for_function("() => window.editor && typeof window.editor.setValue === 'function'")
-
-        data = """
-        maia.avance()
-        maia.avance()
-        maia.direita()
-
-        maia.avance()
-        maia.avance()
-        maia.direita()
-
-        maia.avance()
-        maia.avance()
-        maia.direita()
-        """
-        data = textwrap.dedent(data)
-        page.evaluate('data => {window.editor.setValue(data)}', data)
-        page.locator("#run-btn").click()
-        page.wait_for_function("() => window.is_running === false && window.command_queue_len === 0")
-
-        output = page.locator("#output-content").inner_text()
-        assert "KeyError" not in output
