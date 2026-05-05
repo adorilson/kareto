39b92d6d5ccd8fa617943403f324cdbf9b092f2a
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2026-05-05 17:24:32 +0100

    Adiciona testes para a classe Abelha

---
+++ web/tests/test_abelha.py
@@ -0,0 +1,139 @@
+import pytest
+
+from web.entities import Abelha, Direcao
+
+
+class DummyRenderer:
+    def __init__(self):
+        self.registered = []
+        self.rendered = []
+
+    def register_actor(self, actor):
+        self.registered.append(actor)
+
+    def render_actor(self, actor):
+        self.rendered.append((actor.x, actor.y, actor.direcao))
+
+
+class DummyWorld:
+    def __init__(self, width=4, height=4):
+        self.width = width
+        self.height = height
+        self._girassois = {}
+
+    def in_bounds(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def add_girassol(self, posicao, girassol):
+        self._girassois[posicao] = girassol
+
+    def girassol_em(self, posicao):
+        if posicao not in self._girassois:
+            raise KeyError("girassol not found")
+        return self._girassois[posicao]
+
+
+class DummyGirassol:
+    def __init__(self):
+        self.extracted = 0
+
+    def extract_nectar(self):
+        self.extracted += 1
+
+
+def make_abelha(x=1, y=1, direcao=Direcao.LESTE, world=None, renderer=None):
+    world = world or DummyWorld()
+    renderer = renderer or DummyRenderer()
+    queue = []
+    return Abelha(world, renderer, queue, x=x, y=y, direcao=direcao), queue
+
+
+def test_init_registers_actor_and_sets_shape_and_zindex():
+    world = DummyWorld()
+    renderer = DummyRenderer()
+    queue = []
+
+    abelha = Abelha(world, renderer, queue, x=2, y=3, direcao=Direcao.NORTE)
+
+    assert renderer.registered == [abelha]
+    assert abelha.z_index == 3
+    assert abelha.shape() == Abelha.IMAGENS[Direcao.NORTE]
+
+
+def test_goto_enqueues_and_moves_and_renders():
+    abelha, queue = make_abelha(x=0, y=0)
+
+    abelha.goto(3, 2)
+
+    assert len(queue) == 1
+    queue.pop(0)()
+    assert abelha.posicao == (3, 2)
+    assert abelha.renderer.rendered[-1][:2] == (3, 2)
+
+
+@pytest.mark.parametrize(
+    "direcao, start, expected",
+    [
+        (Direcao.LESTE, (1, 1), (2, 1)),
+        (Direcao.OESTE, (2, 1), (1, 1)),
+        (Direcao.NORTE, (1, 2), (1, 1)),
+        (Direcao.SUL, (1, 1), (1, 2)),
+    ],
+)
+def test_avance_moves_and_renders(direcao, start, expected):
+    world = DummyWorld(width=4, height=4)
+    renderer = DummyRenderer()
+    queue = []
+    abelha = Abelha(
+        world, renderer, queue, x=start[0], y=start[1], direcao=direcao
+    )
+
+    abelha._avance()
+
+    assert abelha.posicao == expected
+    assert renderer.rendered[-1][:2] == expected
+
+
+def test_avance_raises_out_of_bounds():
+    world = DummyWorld(width=2, height=2)
+    renderer = DummyRenderer()
+    queue = []
+    abelha = Abelha(world, renderer, queue, x=1, y=0, direcao=Direcao.LESTE)
+
+    with pytest.raises(ValueError):
+        abelha._avance()
+
+
+def test_direita_enqueues_and_rotates():
+    abelha, queue = make_abelha(direcao=Direcao.LESTE)
+
+    abelha.direita()
+
+    assert len(queue) == 1
+    queue.pop(0)()
+    assert abelha.heading() == Direcao.SUL
+
+
+def test_esquerda_enqueues_and_rotates():
+    abelha, queue = make_abelha(direcao=Direcao.LESTE)
+
+    abelha.esquerda()
+
+    assert len(queue) == 1
+    queue.pop(0)()
+    assert abelha.heading() == Direcao.NORTE
+
+
+def test_extraia_nectar_enqueues_and_extracts():
+    world = DummyWorld()
+    renderer = DummyRenderer()
+    queue = []
+    abelha = Abelha(world, renderer, queue, x=1, y=1, direcao=Direcao.LESTE)
+    girassol = DummyGirassol()
+    world.add_girassol((1, 1), girassol)
+
+    abelha.extraia_nectar()
+
+    assert len(queue) == 1
+    queue.pop(0)()
+    assert girassol.extracted == 1
