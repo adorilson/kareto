ec8654474a1cad9fbe373b56c0c0e19eae8db5ba
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2026-06-13 21:52:42 +0100

    Adiciona teste pra envio de snapshot

---
+++ web/tests/test_envio_snapshot.py
@@ -0,0 +1,37 @@
+from playwright.sync_api import sync_playwright
+
+from common import monitor_dialogs, wait_for_output_content, fail_on_console_errors
+
+
+def test_envio_snapshot(request):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
+        page = browser.new_page()
+
+        request.node.console_attach(page)
+        dialog_state = monitor_dialogs(page)
+
+        # Com mudanças recentes, o teste passou a falhar com o fast=1 aqui.
+        # Talvez devido os timers para processamentos da fila e testes
+        # pós-execução.
+        page.goto("http://localhost:8000/?maia=1,4,0&cag=1&gs=2,4")
+        page.wait_for_function("() => document.getElementById('loading-overlay').className == 'hidden'")
+
+        assert page.locator('.CodeMirror').is_visible()
+        data = """maia.avance()"""
+        page.evaluate('data => {window.editor.setValue(data)}', data)
+
+        with page.expect_response("*codesnapshotweb.php*") as api:
+            page.locator("#run-btn").click()
+
+            page.wait_for_function("() => window.is_running === false && window.command_queue_len === 0")
+
+            SUCESSO = "Tarefa realizada com sucesso."
+            wait_for_output_content(page, SUCESSO)
+            assert dialog_state["dialogs"] == []
+
+            status = api.value.status
+            response = api.value.json()
+            assert response["status"] == "ok", f"API response status not ok: {response}"
+            assert status == 200, f"API call failed with status {status}"
+
