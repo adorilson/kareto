30028f78f909fe4a9487ede1326358dff2457bfc
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2026-06-13 22:56:19 +0100

    Adiciona teste para dupla execução de snapshot com codigo igual

--- web/tests/test_envio_snapshot.py
+++ web/tests/test_envio_snapshot.py
@@ -1,6 +1,15 @@
+import pytest
+
 from playwright.sync_api import sync_playwright
+from playwright._impl._api_types import TimeoutError
 
 from common import monitor_dialogs, wait_for_output_content, fail_on_console_errors
+
+def assert_api_snapshot_response(response):
+    status = response.value.status
+    json = response.value.json()
+    assert json["status"] == "ok", f"API response status not ok: {json}"
+    assert status == 200, f"API call failed with status {status}"
 
 
 def test_envio_snapshot(request):
@@ -34,4 +43,40 @@ def test_envio_snapshot(request):
             response = api.value.json()
             assert response["status"] == "ok", f"API response status not ok: {response}"
             assert status == 200, f"API call failed with status {status}"
+
+
+def test_dupla_execucao_com_codigo_igual(request):
+    """No caso de reexecução, com mesmo código e mesmo resultado, não deve ser
+    feito novo envio de snapshot."""
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, args=["--start-maximized"],)
+        page = browser.new_page()
+
+        request.node.console_attach(page)
+        dialog_state = monitor_dialogs(page)
+
+        # Com mudanças recentes, o teste passou a falhar com o fast=1 aqui.
+        # Talvez devido os timers para processamentos da fila e testes
+        # pós-execução.
+        page.goto("http://localhost:8000/?maia=1,4,0&cag=1&gs=2,4")
+        page.wait_for_function("() => document.getElementById('loading-overlay').className == 'hidden'")
+
+        assert page.locator('.CodeMirror').is_visible()
+        data = """maia.avance()"""
+        page.evaluate('data => {window.editor.setValue(data)}', data)
+
+        SUCESSO = "Tarefa realizada com sucesso."
+        with page.expect_response("*codesnapshotweb.php*") as api:
+            page.locator("#run-btn").click()
+
+            page.wait_for_function("() => window.is_running === false && window.command_queue_len === 0")
+
+            wait_for_output_content(page, SUCESSO)
+
+        # Reexecutando para testar se é feito novo enviou ou não.
+        with pytest.raises(TimeoutError):
+            with page.expect_response("*codesnapshotweb.php*", timeout=2000) as api:
+                page.locator("#run-btn").click()
+                assert_api_snapshot_response(api)
+
 
